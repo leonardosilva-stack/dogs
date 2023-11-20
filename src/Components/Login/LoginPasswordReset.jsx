1d6ee8855aac625099e1521d0f5a2836a77ed671
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2023-11-20 04:29:34 +0000

    Deploy to production

--- src/Components/Login/LoginPasswordReset.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function LoginPasswordReset() {
-  return (
-    <div>LoginPasswordReset</div>
-  )
-}
-
-export default LoginPasswordReset
--- src/Components/User/UserStats.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UserStats = () => {
-  return <div>Estatísticas</div>;
-};
-
-export default UserStats;
