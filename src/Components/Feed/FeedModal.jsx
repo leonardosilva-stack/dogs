ddba915d2dfaecbc8aa3931bb0c18d671d485133
Leonardo Rosa da Silva <leonardo.rosa.silva@outlook.com.br>
2024-04-14 12:05:13 -0300

    style: FeedModal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -5,8 +5,9 @@ import { PHOTO_GET } from "../../api";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
 import PhotoContent from "../Photo/PhotoContent";
+import { func } from "prop-types";
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
 
   React.useEffect(() => {
@@ -14,8 +15,12 @@ const FeedModal = ({ photo }) => {
     request(url, options);
   }, [photo, request]);
 
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
